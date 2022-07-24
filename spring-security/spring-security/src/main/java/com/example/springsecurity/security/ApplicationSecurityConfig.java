7079f9a68537b0191d020dac57d8eb4bd8403a11
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2022-07-24 15:41:41 +0600

    create security roles and permissions

--- spring-security/spring-security/src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ spring-security/spring-security/src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,26 +1,62 @@
 package com.example.springsecurity.security;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.springsecurity.security.ApplicationUserRole.ADMIN;
+import static com.example.springsecurity.security.ApplicationUserRole.STUDENT;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+                        .antMatchers("/","/js/*","/css/*","index")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    protected InMemoryUserDetailsManager userDetailsService() {
+        UserDetails anaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())
+                .build();
+
+        UserDetails linda = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password"))
+                .roles(ADMIN.name())
+                .build();
+        return new InMemoryUserDetailsManager(anaSmithUser, linda);
+    }
+
+
 }
 //public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //    @Override
