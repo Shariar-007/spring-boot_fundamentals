5f179337a8b2ea0d33a80c177c51de386fb11eb4
Shariar-007 <uiu.151007@gmail.com>
2022-07-24 01:36:09 -0700

    basic security topic learning [topic: Spring Security]

---
+++ spring-security/spring-security/src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
+//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+//    }
+//}
