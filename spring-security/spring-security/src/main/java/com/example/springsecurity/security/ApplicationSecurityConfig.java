aa7e6eba7011ecae63d3fbbb94aa4f8dc2bcb7e6
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2022-07-27 16:16:54 +0600

    applied role based access using authorities keyword

--- spring-security/spring-security/src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ spring-security/spring-security/src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ package com.example.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.example.springsecurity.security.ApplicationUserPermission.STUDENT_WRITE;
 import static com.example.springsecurity.security.ApplicationUserRole.*;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -29,15 +32,21 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests((authz) -> authz
                         .antMatchers("/","/js/*","/css/*","index")
                         .permitAll()
                         .antMatchers("/api/**").hasRole(STUDENT.name())
+                        .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                        .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                        .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                        .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                         .anyRequest()
                         .authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
+        //                        .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyAuthority(COURSE_WRITE.name(), STUDENT_WRITE.name())
     }
 
     @Bean
@@ -45,19 +54,22 @@ public class ApplicationSecurityConfig {
         UserDetails anaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+//                .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails linda = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+//                .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tom = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMINTRAINEE.name())
+//                .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(anaSmithUser, linda, tom);
--- spring-security/spring-security/src/main/java/com/example/springsecurity/security/ApplicationUserRole.java
+++ spring-security/spring-security/src/main/java/com/example/springsecurity/security/ApplicationUserRole.java
@@ -1,15 +1,16 @@
 package com.example.springsecurity.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.springsecurity.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
     STUDENT(Sets.newHashSet()),
     ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE)),
-
     ADMINTRAINEE(Sets.newHashSet(COURSE_READ,STUDENT_READ));
 
 
@@ -21,5 +22,13 @@ public enum ApplicationUserRole {
 
     public Set<ApplicationUserPermission> getPermissions(){
         return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions1 = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions1.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions1;
     }
 }
