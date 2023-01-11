137851a222a5197e448585c38022f038072b873a
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2023-01-11 14:03:09 +0600

    p:flywayDb-practice1 => using flyway, practiced migration with springboot application

---
+++ flywayDB-practice1/src/main/resources/db/migration/V2__updateTable.sql
@@ -0,0 +1,3 @@
+
+ALTER TABLE REGISTRATION_USERS
+ADD column mobile varchar(15) AFTER first_name;
---
+++ flywayDB-practice1/src/main/resources/db/migration/V4__insertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO REGISTRATION_USERS (username, first_name, mobile, email) VALUES ('shariar', 'md', '01671111111', 'abc@gmail.com');
+
+INSERT INTO REGISTRATION_USERS (username, first_name, mobile, email) VALUES ('shohag', 'md', '01672222222', 'def@gmail.com');
